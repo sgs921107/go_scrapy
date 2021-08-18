b9a4a4181940fbd8bbbf959d5602368c83eda7df
sgs921107 <757513128@qq.com>
2021-08-18 13:48:51 +0800

    update depends

--- go.mod
+++ go.mod
@@ -12,13 +12,13 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/lestrrat-go/strftime v1.0.5 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
-	github.com/sgs921107/gcommon v0.0.0-20210817132131-d779eee1f981
+	github.com/sgs921107/gcommon v0.0.0-20210818040901-896a1917be8d
 	github.com/sgs921107/glogging v0.0.0-20201223091949-fb29e1042839
 	github.com/sgs921107/gredis v0.0.0-20210731094202-dc7fb7ec6bce
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
-	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
+	golang.org/x/sys v0.0.0-20210817190340-bfb29a6856f2 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- vendor/github.com/sgs921107/gcommon/thread_group.go
+++ vendor/github.com/sgs921107/gcommon/thread_group.go
@@ -1,7 +1,7 @@
 package gcommon
 
 import (
-	"time"
+	"sync"
 )
 
 // Group wait group
@@ -10,20 +10,22 @@ type Group interface {
 	Len() int
 	Add(num int)
 	Done()
-	Wait(inrterval time.Duration, timeout ...time.Duration)
+	Wait()
 }
 
 // ThreadGroup thread group
 type ThreadGroup struct {
 	size      int
-	groupChan chan interface{}
+	groupChan chan struct{}
+	wg      *sync.WaitGroup
 }
 
 func (p *ThreadGroup) init() {
-	if p.size == 0 {
+	if p.size <= 0 {
 		p.size = 1
 	}
-	p.groupChan = make(chan interface{}, p.size)
+	p.groupChan = make(chan struct{}, p.size)
+	p.wg = &sync.WaitGroup{}
 }
 
 // Size size
@@ -39,54 +41,27 @@ func (p *ThreadGroup) Len() int {
 // Add add thread num
 func (p *ThreadGroup) Add(num int) {
 	for i := 0; i < num; i++ {
-		p.groupChan <- 1
+		p.groupChan <- struct{}{}
+		p.wg.Add(1)
 	}
 }
 
 // Done done
 func (p *ThreadGroup) Done() {
 	<-p.groupChan
+	p.wg.Add(-1)
 }
 
 // Wait wait
-// interval 指定间隔多久检测一次是否所有任务都已完成 默认：100纳秒
-// timeout为可选参数，如果大于0则等待指定的时间后退出
-func (p *ThreadGroup) Wait(interval time.Duration, timeout ...time.Duration) {
-	if interval == 0 {
-		interval = 100
-	}
-	if len(timeout) == 0 || timeout[0] == 0 {
-		ticker := time.NewTicker(interval)
-		for {
-			select {
-			case <-ticker.C:
-				if len(p.groupChan) == 0 {
-					goto Break
-				}
-			}
-		}
-	} else {
-		ticker := time.NewTicker(interval)
-		timer := time.NewTimer(timeout[0])
-		for {
-			select {
-			case <-ticker.C:
-				if len(p.groupChan) == 0 {
-					goto Break
-				}
-			case <-timer.C:
-				goto Break
-			}
-		}
-	}
-Break:
+func (p *ThreadGroup) Wait() {
+	p.wg.Wait()
 	close(p.groupChan)
 }
 
 // NewThreadGroup new a thread group
-func NewThreadGroup(size uint) Group {
+func NewThreadGroup(size int) Group {
 	group := &ThreadGroup{
-		size: int(size),
+		size: size,
 	}
 	group.init()
 	return group
